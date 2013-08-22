b560b1eddc5dea76b2559ba024c724d9d90b4b53
Nandini Goel <nandinigoel.india@gmail.com>
2013-08-22 11:02:12 +0530

    Made some Changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,14 @@ validates :first_name, presence: true
   has_many :posts
 has_many :user_friendships
 has_many :friends, through: :user_friendships
+                   conditions: { user_friendships: { state: 'accepted' } }
+
+has_many :pending_user_friendships, class_name: 'UserFriendship',
+                                    foreign_key: :user_id,
+conditions: { state: 'pending' }
+
+has_many :pending_friends, through: :pending_user_friendships, source: :friend
+
 
   
 def to_param
